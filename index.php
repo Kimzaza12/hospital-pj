a1bca001b300df894acc88f453483aa07323ef56
Kimzaza12 <gamekak12@gmail.com>
2023-05-10 10:15:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>ระบบจองห้องพักออนไลน์</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <h1> Hello world and kimzaza12</h1>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">หน้าหลัก</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">จองห้องพัก</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ตรวจสอบการจอง</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="container-fluid p-5 bg-primary text-white text-center">
+  <h1>ระบบจองห้องพักผู้ป่วย</h1>
+  <p>ยินดีต้อนรับสู่ระบบจองห้องพักโรงพยาบาล</p> 
+</div>
+
+<div class="row">
+    <div class="col-sm-4" style="background-color:#4f4f4f;">
+    <h5> เมนูหลัก </h5>    
+    </div>
+
+    <div class="col-sm-8" style="background-color:">ยินดีต้อนรับสู่ระบบจองห้องพักโรงพยาบาล</div>
+
+  </div>
+
+
+
+
+
+
+
+
+
 </body>
 </html>
